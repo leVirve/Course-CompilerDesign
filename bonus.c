17e2c8a341bd4d7fff5a18ee88d0672951b96a95
Salas <ga.e.m.project@gmail.com>
2015-04-01 11:59:24 +0800

    bonus if,while

--- bonus.c
+++ bonus.c
@@ -4,7 +4,23 @@ int main()
 {
     int a = 0;
     for(int i = 0; i <= 3; i++) {
-        a = a + i;
+        if(a <= 5) a = a + i;
+        else {
+            a = 0;
+            i = 99;
+        }
     }
+
+
+    int x = 0, y = 10;
+
+    while(x != y) {
+        x++;
+        y--;
+        if(x == y) a = 100;
+    }
+
+    char* str = "hello, world";
+
     return 0;
 }
